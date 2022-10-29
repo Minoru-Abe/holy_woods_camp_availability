66c13380f69cf427bb9f69b1d9f183fbfb925097
Minoru Abe <Tomominoru.abe@gmail.com>
2022-10-29 14:26:15 +0900

    added a batch file to execute python with appropriate anaconda env

---
+++ holy_woods_camp_availability_check.bat
@@ -0,0 +1,14 @@
+rem change character code to UTF-8 (When the path doesn't include Japanese this is not necessary, but will do this very just in case)
+chcp 65001
+
+rem to activate target anaconda virtual environment
+call C:\Users\tomom\anaconda3\Scripts\activate.bat
+
+rem activate target environment which is holy_woods_camp_availability
+call activate holy_woods_camp_availability
+
+rem execute python with target env's python.exe
+C:\Users\tomom\anaconda3\envs\holy_woods_camp_availability\python.exe holy_woods_camp_availability_check.py False
+
+rem to keep bat window
+pause
